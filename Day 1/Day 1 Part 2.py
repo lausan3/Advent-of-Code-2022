0458d93fb093ba175723fe9757b847f1f952079d
lausan3 <92795688+lausan3@users.noreply.github.com>
2022-12-21 13:19:14 -0500

    + Day 1 Part 2

---
+++ Day 1/Day 1 Part 2.py
@@ -0,0 +1,26 @@
+sum = 0
+biggestSum = [0, 0, 0];
+
+file = open('input.txt', 'r')
+lines = file.readlines()
+
+#read input
+for line in lines:
+  if (line == '\n'):
+    sum = 0
+    continue
+
+  sum += int(line)
+
+  i = 0
+
+  #if current sum is bigger than an index in the array biggestSum, replace it and go to the next line
+  for l in biggestSum:
+    if sum > biggestSum[i]:
+      biggestSum[i] = sum
+      break
+    i += 1
+
+print(biggestSum[0] + biggestSum[1] + biggestSum[2])
+
+file.close()
